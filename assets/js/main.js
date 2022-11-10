a137f02b0dcc7380c0020caa7888d736ecd09d51
hoang vu <hoangvx@appota.com>
2022-11-10 21:33:12 +0700

    update

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-  window.setTimeout(() => {
-    $("#SPLoading").fadeOut(300);
-    $("body").removeClass("overflow-hidden");
-  }, 800);
-  $(window).on("scroll", function () {
-    setHeaderClass();
-  });
-});
